a58379908c0f865670adbc6de33f1446cc2eba02
SAGRSURI <sagarsuriofficial@gmail.com>
2024-08-06 13:11:55 +0530

    V.0

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/testing/" ,
+  resolve: {
+    alias: {
+      assets: '/src/assets', // Use absolute path
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.mp3'],
+  },
+});
